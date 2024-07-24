14589adc4c48411a2ef414a223ecf04a33fc31fb
quvinh <vinhhp2620@gmail.com>
2024-07-24 18:25:39 +0700

    upload wh location

--- app/Models/Cell.php
+++ app/Models/Cell.php
@@ -9,8 +9,8 @@ use App\Traits\UUID;
 
 class Cell extends Model
 {
-    use HasFactory,UUID;
-    protected $fillable = ['id','note','name','sheft_id','number_of_bin','product_id'];
+    use HasFactory;
+    protected $fillable = ['id','note','name','sheft_id','number_of_bin','product_id','warehouse_id'];
     protected $hidden=['created_at','updated_at','id','sheft_id','note'];
     public function lot(){
         return $this->belongsToMany(Lot::class,'cell_lot','cell_id','lot_id');
