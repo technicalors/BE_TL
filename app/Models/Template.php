da72861a78935bab0b1823f1c7706d2b7acfb937
quvinh <vinhhp2620@gmail.com>
2024-07-26 22:55:45 +0700

    template

---
+++ app/Models/Template.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Validator;
+
+class Template extends Model
+{
+    use HasFactory;
+    protected $table = "templates";
+    protected $fillable = ['material_id', 'quantity'];
+    protected $casts = [
+        'material_id' => 'string',
+    ];
+
+    static function validate($input, $id = null)
+    {
+        $validated = Validator::make(
+            $input,
+            [
+                'material_id'=>'required',
+                'quantity'=>'required|integer|min:0',
+            ],
+            [
+                'material_id.required'=>'Vui lòng nhập NVL',
+                'quantity.required'=>'Vui lòng nhập số lượng',
+            ]
+        );
+        return $validated;
+    }
+}
