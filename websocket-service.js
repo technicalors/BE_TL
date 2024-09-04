0e26b96ccc3ed5a6e808449e7064745eaa70aa18
huutran96hy <80299380+huutran96hy@users.noreply.github.com>
2024-09-04 07:51:31 +0700

    1

--- websocket-service.js
+++ websocket-service.js
@@ -285,6 +285,10 @@ async function enqueueData(deviceId, data) {
                 const result = await axios.get("http://103.77.215.18:3030/api/plugins/telemetry/DEVICE/" + deviceId + "/values/timeseries?keys=PLC:Num_Input,PLC:Num_Out", { headers: { 'Authorization': 'Bearer ' + authToken } });
                 convertedData.input = result.data['PLC:Num_Input'] ? result.data['PLC:Num_Input'][0]['value'] : 0;
                 convertedData.output = result.data['PLC:Num_Out'][0]['value'];
+                if(deviceId == '22d821e0-45bd-11ef-b8c3-a13625245eca'){
+                    console.log('convertedData',convertedData);
+                    console.log('Data Counter',data['PLC:Count_En'][0][1]);
+                }
                 dataQueues[deviceId].push({ data: convertedData, apiUrl: MACHINE_RECORD_API_URL });
             }
             lastMachineRecordValues[deviceId] = data;
