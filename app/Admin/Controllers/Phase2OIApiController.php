9260ace65a90f04db30eb9afeb880254e9d2efd4
huutran96hy <80299380+huutran96hy@users.noreply.github.com>
2024-10-29 18:18:19 +0700

    1

--- app/Admin/Controllers/Phase2OIApiController.php
+++ app/Admin/Controllers/Phase2OIApiController.php
@@ -194,8 +194,8 @@ class Phase2OIApiController extends Controller
             ->whereHas('plan', function ($q) {
                 $q->whereIn('status_plan', [0, 1]);
             })
-            ->orWhereHas('infoCongDoan', function ($q) {
-                $q->where('status', 1);
+            ->orWhereHas('infoCongDoan', function ($q) use ($machine_code) {
+                $q->where('status', 1)->where('machine_code', $machine_code);
             })
             ->with('infoCongDoan.qcHistory', 'spec', 'plan', 'infoCongDoan.assignments');
         if (!empty($request->line_id)) {
