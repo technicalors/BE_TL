09e45181ff3882afa4874554097aa6432c03f50f
quvinh <vinhhp2620@gmail.com>
2024-11-08 16:16:37 +0700

    checkParameter

--- app/Models/LogWarningParameter.php
+++ app/Models/LogWarningParameter.php
@@ -16,50 +16,60 @@ class LogWarningParameter extends Model
     {
         try {
             $params = (array) $request->all();
-            $scenarios = Scenario::all();
-            $mark = [];
-            foreach ($scenarios as $item) {
-                $mark[$item->parameter_id] = $item;
+            $machine_id = $request->machine_id ?? null;
+            if (isset($request->device_id)) {
+                $machine = Machine::where('device_id', $request->device_id)->first();
+                if (!empty($machine)) {
+                    $machine_id = $machine->id;
+                }
             }
-            foreach ($params as $key => $value) {
-                if (isset($mark[$key])) {
-                    $tm = $mark[$key];
-                    $f1 = (float)$value > (float)$tm->tieu_chuan_kiem_soat_tren;
-                    $f2 = (float)$value < (float)$tm->tieu_chuan_kiem_soat_duoi;
-                    $f3 = $value != -1;
-                    $f4 = (float)$value <= (float)$tm->tieu_chuan_kiem_soat_tren;
-                    $f5 = (float)$value >= (float)$tm->tieu_chuan_kiem_soat_duoi;
-                    if (($f1 || $f2) && $f3) {
-                        $check_log = LogWarningParameter::where('parameter_id', $key)->first();
-                        if ($check_log) {
-                            $check_monitor = Monitor::where('parameter_id', $key)->where('machine_id', $request->machine_id)->where('status', 0)->first();
-                            if ($check_monitor) {
-                                $check_monitor->update(['content' => $tm->hang_muc . ': ' . $value]);
+
+            if (isset($machine_id)) {
+                $scenarios = Scenario::all();
+                $mark = [];
+                foreach ($scenarios as $item) {
+                    $mark[$item->parameter_id] = $item;
+                }
+                foreach ($params as $key => $value) {
+                    if (isset($mark[$key])) {
+                        $tm = $mark[$key];
+                        $f1 = (float)$value > (float)$tm->tieu_chuan_kiem_soat_tren;
+                        $f2 = (float)$value < (float)$tm->tieu_chuan_kiem_soat_duoi;
+                        $f3 = $value != -1;
+                        $f4 = (float)$value <= (float)$tm->tieu_chuan_kiem_soat_tren;
+                        $f5 = (float)$value >= (float)$tm->tieu_chuan_kiem_soat_duoi;
+                        if (($f1 || $f2) && $f3) {
+                            $check_log = LogWarningParameter::where('parameter_id', $key)->first();
+                            if ($check_log) {
+                                $check_monitor = Monitor::where('parameter_id', $key)->where('machine_id', $machine_id)->where('status', 0)->first();
+                                if ($check_monitor) {
+                                    $check_monitor->update(['content' => $tm->hang_muc . ': ' . $value]);
+                                } else {
+                                    $monitor = new Monitor();
+                                    $monitor->type = 'cl';
+                                    $monitor->content =  $tm->hang_muc;
+                                    $monitor->value =  $value;
+                                    $monitor->parameter_id = $key;
+                                    $monitor->machine_id = $machine_id;
+                                    $monitor->status = 0;
+                                    $monitor->save();
+                                }
                             } else {
-                                $monitor = new Monitor();
-                                $monitor->type = 'cl';
-                                $monitor->content =  $tm->hang_muc;
-                                $monitor->value =  $value;
-                                $monitor->parameter_id = $key;
-                                $monitor->machine_id = $request->machine_id;
-                                $monitor->status = 0;
-                                $monitor->save();
+                                $log = new LogWarningParameter();
+                                $log->parameter_id = $key;
+                                $log->value = $value;
+                                $log->machine_id = $machine_id;
+                                $log->save();
                             }
-                        } else {
-                            $log = new LogWarningParameter();
-                            $log->parameter_id = $key;
-                            $log->value = $value;
-                            $log->machine_id = $request->machine_id;
-                            $log->save();
+                        } elseif ($f4 && $f5 && $f3) {
+                            LogWarningParameter::where('parameter_id', $key)->where('machine_id', $machine_id)->delete();
+                            Monitor::where('parameter_id', $key)->where('machine_id', $machine_id)->where('status', 0)->update(['status' => 1]);
                         }
-                    } elseif ($f4 && $f5 && $f3) {
-                        LogWarningParameter::where('parameter_id', $key)->where('machine_id', $request->machine_id)->delete();
-                        Monitor::where('parameter_id', $key)->where('machine_id', $request->machine_id)->where('status', 0)->update(['status' => 1]);
                     }
                 }
             }
         } catch (\Exception $e) {
-            // Log::error('🛑 ' . $e->getMessage());
+            Log::error('🛑 ' . $e->getMessage());
         }
     }
 }
