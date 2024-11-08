900bdb1cf51b378dd9e5f30cbd5913de3faa3070
quvinh <vinhhp2620@gmail.com>
2024-11-08 16:57:44 +0700

    fix

--- app/Admin/Controllers/MaterialParameterLogController.php
+++ app/Admin/Controllers/MaterialParameterLogController.php
@@ -3,6 +3,7 @@
 namespace App\Admin\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\LogWarningParameter;
 use App\Models\MachineParameterLogs;
 use Encore\Admin\Controllers\Dashboard;
 use Encore\Admin\Layout\Column;
@@ -13,7 +14,8 @@ class MaterialParameterLogController extends Controller
 {
     public function index()
     {
-        $result = MachineParameterLogs::orderByDesc('created_at')->with('machineParameter.scenario')->get()->take(20);
+        // $result = MachineParameterLogs::orderByDesc('created_at')->with('machineParameter.scenario')->get()->take(20);
+        $result = LogWarningParameter::orderByDesc('updated_at')->where('machine_id', 'e281ee2c-0b50-404b-9c5f-c082dc655d64')->get();
         return response()->json(['data' => $result], 200);
     }
 }
