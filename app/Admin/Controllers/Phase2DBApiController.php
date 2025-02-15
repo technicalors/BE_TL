90853e028bcaf6ac95b2c6d515d7a4e642eb3089
huutran96hy <80299380+huutran96hy@users.noreply.github.com>
2025-02-15 08:45:26 +0700

    1

--- app/Admin/Controllers/Phase2DBApiController.php
+++ app/Admin/Controllers/Phase2DBApiController.php
@@ -240,10 +240,10 @@ class Phase2DBApiController extends Controller
                     "status" => $status,
                     "time" => $info->updated_at,
                 ];
-                $tm['ti_le_ht'] = (int) (100 * (($tm['sl_dau_ra_kh']) > 1 ? number_format(($tm['sl_thuc_te'] / ($tm['sl_dau_ra_kh'])), 2) : 0));
-                if ($tm['ti_le_ht'] > 100) {
+                // $tm['ti_le_ht'] = (int) (100 * (($tm['sl_dau_ra_kh']) > 1 ? number_format(($tm['sl_thuc_te'] / ($tm['sl_dau_ra_kh'])), 2) : 0));
+                // if ($tm['ti_le_ht'] > 100) {
                     $tm['ti_le_ht'] = 100;
-                }
+                // }
                 $data[] = $tm;
             }
         }
