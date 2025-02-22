555bfa716b8b872be49504691d98b66674717ed6
huutran96hy <80299380+huutran96hy@users.noreply.github.com>
2025-02-22 08:05:48 +0700

    1

--- app/Admin/Controllers/Phase2DBApiController.php
+++ app/Admin/Controllers/Phase2DBApiController.php
@@ -298,21 +298,24 @@ class Phase2DBApiController extends Controller
             $info = InfoCongDoan::where("line_id", $machine->line_id)->where('machine_code', $machine->code)->with(["lotPlan", "lot.plan.product"])->whereDate('thoi_gian_bat_dau', date('Y-m-d'))->orderBy('thoi_gian_bat_dau', 'DESC')->first();
             $tracking = Tracking::where('machine_id', $machine->code)->first();
             if (!$info) {
-                // $tm = [
-                //     "cong_doan" => mb_strtoupper($machine->line->name, 'UTF-8'),
-                //     'machine_code' => $machine->code,
-                //     'machine_name' => $machine->code,
-                //     "product" => '',
-                //     "sl_dau_ra_kh" => $info->lotPlan->quantity ?? 0,
-                //     "sl_thuc_te" => 0,
-                //     "sl_muc_tieu" => 0,
-                //     "ti_le_ng" => 0,
-                //     "ti_le_ht" => 0,
-                //     "status" => 0,
-                //     "time" => "",
-                //     'ti_le_ht' => 0
-                // ];
-                // $data[] = $tm;
+                $plan = ProductionPlan::where('line_id', $machine->line_id)
+                    ->where('machine_id', $machine->code)
+                    ->whereDate('thoi_gian_bat_dau', date('Y-m-d'))
+                    ->first();
+                $tm = [
+                    "cong_doan" => mb_strtoupper($plan->line->name, 'UTF-8'),
+                    'machine_code' => $machine->code,
+                    'machine_name' => $machine->code,
+                    "product" => $plan ? $plan->product->name : '',
+                    "sl_dau_ra_kh" => $plan->sl_giao_sx ?? 0,
+                    "sl_thuc_te" => 0,
+                    "sl_muc_tieu" =>  $sumLotPlan ?? 0,
+                    "ti_le_ng" => 0,
+                    "ti_le_ht" => 0,
+                    "status" => 0,
+                    "time" => "",
+                ];
+                $data[] = $tm;
             } else {
                 // $lotPlan = $info->lotPlan;
                 // if (!$lotPlan) {
