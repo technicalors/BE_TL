9e87b12c073adb46979492875a3c474de85e68a8
huutran96hy <80299380+huutran96hy@users.noreply.github.com>
2025-02-23 13:31:50 +0700

    1

--- app/Admin/Controllers/Phase2OIApiController.php
+++ app/Admin/Controllers/Phase2OIApiController.php
@@ -332,8 +332,7 @@ class Phase2OIApiController extends Controller
     {
         $line_id = $request->line_id;
         $machine_code = $request->machine_code;
-        $date  = date('Y-m-d', strtotime('3 days ago'));
-        $info_query = InfoCongDoan::orderBy('thoi_gian_bat_dau', 'DESC')->whereDate('thoi_gian_bat_dau', $date);
+        $info_query = InfoCongDoan::orderBy('thoi_gian_bat_dau', 'DESC')->whereDate('thoi_gian_bat_dau', date('Y-m-d'));
         if (!empty($request->line_id)) {
             $info_query->where('line_id', $line_id);
         }
