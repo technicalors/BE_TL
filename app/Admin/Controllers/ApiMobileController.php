8c052f33e7220e3939d202e40c563ddca8606209
NDH <nghai2k@gnail.com>
2025-05-21 09:30:53 +0700

    1

--- app/Admin/Controllers/ApiMobileController.php
+++ app/Admin/Controllers/ApiMobileController.php
@@ -4332,7 +4332,7 @@ class ApiMobileController extends AdminController
     function getMachineParameters(Request $request)
     {
         $machine = Machine::with(['parameters' => function ($query) {
-            $query->select('parameters.*', 'machine_parameters.is_if');
+            $query->select('parameters.*', 'machine_parameters.is_if')->orderBy('is_if', 'asc');
         }])->where('code', $request->machine_id)->first();
         $columns = [];
         foreach ($machine->parameters as $param) {
