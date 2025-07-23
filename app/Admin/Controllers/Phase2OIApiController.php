75c9214008af3aa4aeaac61bd87b6fba121381f6
NDH <nghai2k@gnail.com>
2025-07-23 10:53:35 +0700

    1

--- app/Admin/Controllers/Phase2OIApiController.php
+++ app/Admin/Controllers/Phase2OIApiController.php
@@ -540,11 +540,11 @@ class Phase2OIApiController extends Controller
             return $this->failure('', 'Chưa có kế hoạch nào đang sản xuất');
         }
         //Kiểm tra xem KH có cho phép không kiểm tra đầu vào không
-        if (!$current_plan->pass_input_lot_id) {
+        if (!$current_plan->pass_input_lot_id || $current_plan->product_id === 'RD176') {
             $hanh_trinh_san_xuat = Spec::where('slug', 'hanh-trinh-san-xuat')->where('product_id', $current_plan->product_id)->whereRaw('value REGEXP "^[0-9]+$"')->orderBy('value')->pluck('value', 'line_id');
             // return in_array($machine->line_id, [24, 25]) && $hanh_trinh_san_xuat[$machine->line_id] == 1;
-            // $material = Material::find($current_plan->product_id);
-            if ((in_array($machine->line_id, [24, 25]) && isset($hanh_trinh_san_xuat[$machine->line_id]) && $hanh_trinh_san_xuat[$machine->line_id] == 1)) {
+            $material = Material::find($current_plan->product_id);
+            if ($material || (in_array($machine->line_id, [24, 25]) && $hanh_trinh_san_xuat[$machine->line_id] == 1)) {
                 $roll = RollMaterial::with(['material.products', 'warehouse_inventory'])->find($request->scanned_lot);
                 if (!$roll) {
                     return $this->failure([], "Không tìm thấy cuộn");
